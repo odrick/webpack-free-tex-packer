22d12dcbfeed91604f245130180c5bc0c1b2ef8c
Alexander Norinchak <dev@playtomax.com>
2018-12-28 14:44:10 +0200

    FS watching

--- index.js
+++ index.js
@@ -1,5 +1,65 @@
 const fs = require('fs');
+const pathModule = require('path');
 const texturePacker = require("free-tex-packer-core");
+
+function isFolder(path) {
+    if(isExists(path)) {
+        return fs.statSync(path).isDirectory();
+    }
+    else {
+        path = fixPath(path);
+        let name = getNameFromPath(path);
+        let parts = name.split(".");
+        return parts.length === 1;
+    }
+}
+
+function isExists(path) {
+    return fs.existsSync(path);
+}
+
+function fixPath(path) {
+    return path.trim().split("\\").join("/");
+}
+
+function getNameFromPath(path) {
+    return path.trim().split("/").pop();
+}
+
+function getExtFromPath(path) {
+    return path.trim().split(".").pop().toLowerCase();
+}
+
+function getFolderFilesList(dir, base="", list=[]) {
+    let files = fs.readdirSync(dir);
+    for(let file of files) {
+        let path = pathModule.resolve(dir,  file);
+        if (isFolder(path) && path.toUpperCase().indexOf('__MACOSX') < 0) {
+            list = getFolderFilesList(path, base + file + "/", list);
+        }
+        else {
+            list.push({
+                name: (base ? base : "") + file,
+                path: path
+            });
+        }
+    }
+
+    return list;
+}
+
+function getSubFoldersList(dir, list=[]) {
+    let files = fs.readdirSync(dir);
+    for(let file of files) {
+        let path = pathModule.resolve(dir,  file);
+        if (isFolder(path) && path.toUpperCase().indexOf('__MACOSX') < 0) {
+            list.push(path);
+            list = getSubFoldersList(path, list);
+        }
+    }
+
+    return list;
+}
 
 class WebpackFreeTexPacker {
 	constructor(src, dest, options=null) {
@@ -12,31 +72,65 @@ class WebpackFreeTexPacker {
 	
 	apply(compiler) {
 		compiler.hooks.emit.tapAsync('WebpackFreeTexPacker', (compilation, callback) => {
-			/*
-			console.log(this.src);
-			console.log(this.dest);
-			console.log(this.options);
 			
-			for(let path of this.src) {
-				compilation.fileDependencies.add(path);
-			}
-			
-			console.log(compilation.fileDependencies);
-			console.log(compilation.contextDependencies);
-			*/
+		    let files = {};
+		    
+		    for(let srcPath of this.src) {
+                let path = fixPath(srcPath);
+                
+                let name = getNameFromPath(path);
+                
+                if(!name || name === '.' || name === '*' || name === '*.*') {
+                    srcPath = srcPath.substr(0, srcPath.length - name.length - 1);
+                    path = fixPath(srcPath);
+                    name = '';
+                }
 
-			/*
-			compilation.assets['filelist.md'] = {
-				source: function() {
-					return filelist;
-				},
-				size: function() {
-					return filelist.length;
-				}
-			};
-			*/
+                if(isFolder(path)) {
+                    if(isExists(srcPath)) {
+                        let list = getFolderFilesList(path, (name ? name + '/' : ''));
+                        for(let file of list) {
+                            files[file.name] = file.path;
+                        }
+                    }
 
-			callback();
+                    compilation.contextDependencies.add(srcPath);
+                    
+                    let subFolders = getSubFoldersList(srcPath);
+                    for(let folder of subFolders) {
+                        compilation.contextDependencies.add(folder);
+                    }
+                }
+                else {
+                    if(isExists(srcPath)) {
+                        files[getNameFromPath(path)] = path;
+                    }
+
+                    compilation.fileDependencies.add(srcPath);
+                }
+            }
+    		    
+            let images = [];
+            let names = Object.keys(files);
+		    for(let name of names) {
+                images.push({path: name, contents: fs.readFileSync(files[name])});
+            }
+
+            texturePacker(images, this.options, (files) => {
+                for(let item of files) {
+                    (function(item, dest) {
+                        compilation.assets[dest + '/' + item.name] = {
+                            source: function() {
+                                return item.buffer;
+                            },
+                            size: function() {
+                                return item.buffer.length;
+                            }
+                        };
+                    })(item, this.dest);
+                }
+                callback();
+            });
 		});
 	}
 }
